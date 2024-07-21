85924d74c66fc754f343abd2af7dfb3fa9a527b2
bofa78 <zsolt.banfalvi78@gmail.com>
2024-07-21 12:00:53 +0200

    Added unit tests

---
+++ IocLifeCyclesExample/Services/IocLifeCyclesExampleService.cs
@@ -0,0 +1,32 @@
+﻿using IocLifeCyclesExample.Interfaces;
+using IocLifeCyclesExample.Models;
+
+namespace IocLifeCyclesExample.Services
+{
+    public class IocLifeCyclesExampleService(
+        ILogger<IocLifeCyclesExampleService> logger,
+        [FromKeyedServices(Constants.ServiceNames.Scoped)] ILifeCycleService scopedServiceA,
+        [FromKeyedServices(Constants.ServiceNames.Scoped)] ILifeCycleService scopedServiceB,
+        [FromKeyedServices(Constants.ServiceNames.Singleton)] ILifeCycleService singletonServiceA,
+        [FromKeyedServices(Constants.ServiceNames.Singleton)] ILifeCycleService singletonServiceB,
+        [FromKeyedServices(Constants.ServiceNames.Transient)] ILifeCycleService transientServiceA,
+        [FromKeyedServices(Constants.ServiceNames.Transient)] ILifeCycleService transientServiceB) : IIocLifeCyclesExampleService
+    {
+        public IocLifeCyclesExampleResponseModel GetGuids()
+        {
+            var result = new IocLifeCyclesExampleResponseModel();
+            result.Services = new()
+            {
+                { nameof(scopedServiceA), scopedServiceA.GetGuid() },
+                { "ScopedServiceB", scopedServiceB.GetGuid() },
+                { "SingletonServiceA", singletonServiceA.GetGuid() },
+                { "SingletonServiceB", singletonServiceB.GetGuid() },
+                { "TransientServiceA", transientServiceA.GetGuid() },
+                { "TransientServiceB", transientServiceB.GetGuid() }
+            };
+
+            logger.LogInformation(message: System.Text.Json.JsonSerializer.Serialize(result));
+            return result;
+        }
+    }
+}
